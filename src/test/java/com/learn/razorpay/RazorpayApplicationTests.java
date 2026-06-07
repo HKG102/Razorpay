ed73114715278a29a8f7fcbc24a2636cd0147605
Harish Gupta <montygupta008@gmail.com>
2026-06-07 14:32:23 +0530

    first commit

---
+++ src/test/java/com/learn/razorpay/RazorpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.razorpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RazorpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

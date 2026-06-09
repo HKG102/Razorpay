64ae3ec3b906ab859c775c23f45a6c6cd7095d87
Harish Gupta <montygupta008@gmail.com>
2026-06-09 11:48:42 +0530

    bugfix

--- src/main/java/com/learn/razorpay/operations/entity/SettlementPayment.java
+++ src/main/java/com/learn/razorpay/operations/entity/SettlementPayment.java
@@ -2,8 +2,6 @@ package com.learn.razorpay.operations.entity;
 
 import jakarta.persistence.*;
 
-import java.util.UUID;
-
 @Entity
 @Table(name = "settlement_payment")
 public class SettlementPayment {
@@ -11,8 +9,8 @@ public class SettlementPayment {
     @EmbeddedId
     private SettlementPaymentId id;
 
-    @MapsId
-    @ManyToOne(fetch = FetchType.LAZY,  optional = false)
+    @MapsId("settlementId")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "settlement_id", nullable = false)
     private Settlement settlement;
 }
